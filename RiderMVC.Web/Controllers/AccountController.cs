d927ccc29ab3d8b11b52a36e2534ff10be272b51
mobileyangli <mobile_yangli@163.com>
2017-01-25 17:49:52 +0800

    solution init

---
+++ RiderMVC.Web/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace RiderMVC.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            return View();
+        }
+    }
+}
---
+++ RiderMVC.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="RiderMVC.Web.Global" %>
